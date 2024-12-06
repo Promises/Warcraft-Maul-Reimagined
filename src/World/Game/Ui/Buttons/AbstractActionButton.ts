9e599491185cf8bbcc49c9881211b8806f838194
Henning Berge <henning.ber@gmail.com>
2024-12-06 21:22:16 +0100

    Fix actionbar icon rendering

--- src/World/Game/Ui/Buttons/AbstractActionButton.ts
+++ src/World/Game/Ui/Buttons/AbstractActionButton.ts
@@ -13,25 +13,39 @@ export abstract class AbstractActionButton {
     constructor(game: WarcraftMaul, name: string, icon: string, x: number, y: number, size: number) {
         this._game = game;
         const gameFrame = Frame.fromOrigin(ORIGIN_FRAME_GAME_UI, 0)!;
+
+        // Create button
         this._buttonHandle = Frame.createType(
             name,
             gameFrame,
             0,
-            'StandardButtonTemplate',
             'BUTTON',
+            'StandardButtonTemplate',
         )!;
 
+        // Create backdrop as child of button
+        this._backdropHandle = Frame.createType(
+            `${name}BackDrop`,
+            this._buttonHandle,  // Make backdrop a child of button
+            0,
+            'BACKDROP',
+            'ButtonBackdropTemplate'
+        )!;
 
-        this._backdropHandle = Frame.createType(`${name}BackDrop`, gameFrame, 0, 'BACKDROP', 'ButtonBackdropTemplate')!
-
+        // Set sizes
         this._buttonHandle.setSize(size, size);
-        this._backdropHandle.setSize(size, size)
-        this._backdropHandle.setTexture(icon, 0, true)
-        // BlzFrameSetTexture(this._buttonHandle, 'uiImport\\CommandButtons\\BTNNone.dds', 0, true);
-        this._buttonHandle.setAlpha(0);
-        this._buttonHandle.setAbsPoint(FRAMEPOINT_CENTER, x, y);
-        this._buttonHandle.setPoint(FRAMEPOINT_CENTER, this._buttonHandle, FRAMEPOINT_CENTER, 0.0, 0.0);
+        this._backdropHandle.setSize(size, size);
 
+        // Set texture
+        this._backdropHandle.setTexture(icon, 0, true);
+
+        // Position the button (backdrop will follow)
+        this._buttonHandle.setAbsPoint(FRAMEPOINT_CENTER, x, y);
+
+        // Don't set alpha to 0
+        // this._buttonHandle.setAlpha(0);
+
+        // Set up click trigger
         this.trig = Trigger.create();
         this.trig.addAction(() => this.clickAction());
         this.trig.triggerRegisterFrameEvent(this._buttonHandle, FRAMEEVENT_CONTROL_CLICK);
